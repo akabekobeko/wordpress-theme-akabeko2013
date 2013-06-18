49f5c4665d2d56dae9ec54d9dec119896da96adc
akabeko <akabekobeko@gmail.com>
2013-06-19 00:43:05 +0900

    #106 header.php のハードコードしていた CSS 読み込みを削除

--- header.php
+++ header.php
@@ -3,10 +3,6 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?php wp_title( '|', true, 'right' ); bloginfo('name'); ?></title>
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.min.css" media="screen">
-	<!--[if IE 7]>
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome-ie7.min.css">
-	<![endif]-->
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" media="screen">
 	<?php if ( is_singular() ) wp_enqueue_script( "comment-reply" ); ?>
 	<?php wp_head(); ?>
