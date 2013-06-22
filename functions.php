678149ba944a740712904df2e972ed2b0e212f8f
akabeko <akabekobeko@gmail.com>
2013-06-23 01:01:21 +0900

    #88 add_editor_style に対応した

--- functions.php
+++ functions.php
@@ -12,6 +12,9 @@ add_theme_support( 'automatic-feed-links' );
 // Reccomended: Post thumbnail
 add_theme_support( 'post-thumbnails' );
 
+// Reccomended: Editor style
+add_editor_style( 'editor_style.css' );
+
 // Reccomended: Sidebar
 register_sidebar( array( 'name'          => __( 'Sidebar', 'akabeko2013' ),
 						 'id'            => 'sidebar-1',
@@ -38,7 +41,7 @@ add_theme_support( 'custom-header',
  */
 function mytheme_stylesheets()
 {
-	wp_enqueue_style( 'font-awesome',     '//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.min.css'     );
+	wp_enqueue_style( 'font-awesome',     '//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.min.css' );
 	wp_enqueue_style( 'font-awesome-ie7', '//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome-ie7.min.css' );
 	$GLOBALS[ 'wp_scripts' ]->add_data( 'font-awesome-ie7', 'conditional', 'IE 7' );
 }
