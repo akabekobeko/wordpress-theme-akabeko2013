2bf565d143dbf04d5b1edd37a813fa43c6d898ef
akabeko <akabekobeko@gmail.com>
2013-11-04 01:33:19 +0900

    #144 viewport を適用してスマホのデバイス表示幅へ適合するようにした

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes();?>>
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
 	<title><?php wp_title( '|', true, 'right' ); bloginfo('name'); ?></title>
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
